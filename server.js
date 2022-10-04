50b71483fc978ecffa0b7f84aaf3fc8f371bfd73
chanmikim <cmkim@4cgate.com>
2022-10-04 16:22:17 +0900

    change

--- server.js
+++ server.js
@@ -1,17 +1,7 @@
 const jsonServer = require('json-server');
-const path = require('path');
-
 const server = jsonServer.create();
-const router = jsonServer.router(path.resolve('./db.json'));
-const middlewares = jsonServer.defaults({
-    static: path.resolve('./build/')
-});
-
+const router = jsonServer.router('db.json');
 const port = process.env.PORT || 3001;
-
-server.use(middlewares);
-
-server.use(jsonServer.bodyParser);
 
 server.use(router);
 server.listen(port, () => {
