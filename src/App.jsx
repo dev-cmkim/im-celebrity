91452502c5eb0119b9c4de3d2710495dab375087
cmkim <cmkim1003@revu.net>
2023-08-24 16:06:22 +0900

    gh-pages bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import {HashRouter, Route, Switch} from "react-router-dom";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import "./App.css";
 import { Project } from "./pages";
 
 const App = () => {
   return (
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <div className="App">
         <Switch>
-          <Route exact path={process.env.PUBLIC_URL + "/"}>  
+          <Route exact path="/">
             <Project />
           </Route>
         </Switch>
       </div>
-    </HashRouter>
+    </BrowserRouter>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {BrowserRouter, HashRouter} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <HashRouter basename={process.env.PUBLIC_URL}>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
